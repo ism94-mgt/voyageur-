0f56f1ffd013634035b345cb6289094ffce143d1
ism94-mgt <islaip243@gmail.com>
2026-04-28 14:49:55 +0200

    Create tsp.cpp

---
+++ tsp.cpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <array>
+#include <string>
+
+const int NMAX = 100;
+
+struct Ville
+{
+    int numero;
+    double x;
+    double y;
+};
+
+using TabVilles = std::array<Ville, NMAX>;
+using MatriceDistances = std::array<std::array<int, NMAX>, NMAX>;
+using Tournee = std::array<int, NMAX>;
+
+struct InstanceTSP
+{
+    std::string nom;
+    int nb_villes;
+    std::string type_distance;
+
+    TabVilles villes;
+    MatriceDistances distances;
+};
+
+struct Solution
+{
+    Tournee ordre;
+    int nb_villes;
+    double longueur;
+};
+
+void initialiser_instance(InstanceTSP & instance);
+
+void afficher_instance(const InstanceTSP & instance);
+
+int distance_entre_villes(const InstanceTSP & instance, int i, int j);
+
+double longueur_tournee(const InstanceTSP & instance, const Solution & solution);
